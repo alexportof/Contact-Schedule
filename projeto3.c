b2a27b3d11eb37136a235054c1b2c69f20a0f6de
Álex Porto <alexportoferreira@hotmail.com>
2019-05-20 23:16:32 -0300

    Add Menu with option 1

--- projeto3.c
+++ projeto3.c
@@ -25,22 +25,96 @@ void libera_lista(Lista *li);
 int *le_arquivo();
 void carrega_arquivo(Lista *li, FILE *fp, struct contato contato);
 void insere_inicio(Lista *li, struct contato contato);
+int carregar_menu();
+void menu(int opcao, Lista *li);
+void criar_contato(Lista *li);
 //int consulta_lista(Lista *li, int pos, struct contato *cont);
 
 int main()
 {
     struct contato contato;
     Lista list;
+    int opcao = 0;
     FILE *fp;
     list = criaListaVazia();
     fp = le_arquivo();
     carrega_arquivo(list, fp, contato);
-    int x = tamanho_lista(list);
-    printf("%d", x);
-    libera_lista(list);
+    while (opcao <= 5)
+    {
+        opcao = carregar_menu();
+        menu(opcao, list);
+    }
     return 0;
 }
+void criar_contato(Lista *li)
+{
+    struct contato contato;
+    char nome[101];
+    char sobrenome[101];
+    char linha[2];
+    char telefone1[6];
+    char telefone2[5];
+    char endereco[100];
+    int cep;
+    char data_nasc[11], mes[11], ano[11];
 
+    printf("Nome: ");
+    scanf("%s", &nome);
+    printf("Sobrenome: ");
+    scanf("%s", &sobrenome);
+    strcat(nome, " ");
+    strcat(nome, sobrenome);
+    strcpy(contato.nome, nome);
+    printf("5 Primeiros Dígitos do Telefone: ");
+    scanf("%s", &telefone1);
+    printf("4 Ultimos Números do Telefone: ");
+    scanf("%s", &telefone2);
+    strcat(telefone1, "-");
+    strcat(telefone1, telefone2);
+    strcpy(contato.telefone, telefone1);
+    printf("Endereco: ");
+    scanf("%s;", &endereco);
+    strcpy(contato.endereco, endereco);
+    printf("CEP: ");
+    scanf("%d", &cep);
+    contato.cep = cep;
+    printf("Dia de Nascimento: ");
+    scanf("%s", &data_nasc);
+    strcat(data_nasc, "/");
+    printf("Mes: ");
+    scanf("%s", &mes);
+    strcat(data_nasc, mes);
+    strcat(data_nasc, "/");
+    printf("Ano: ");
+    scanf("%s", &ano);
+    strcat(data_nasc, ano);
+    strcpy(contato.data_nasc, data_nasc);
+    insere_inicio(li, contato);
+}
+
+void menu(int opcao, Lista *li)
+{
+    switch (opcao)
+    {
+    case 1:
+        criar_contato(li);
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+        libera_lista(li);
+        break;
+    }
+}
+int carregar_menu()
+{
+    int opcao;
+    printf("-----------------------------\nMenu Da Agenda de Contatos\n-----------------------------\n");
+    printf("1 - Inserir Novo Registro\n2 - Remover Registros Que Contenham Certa String no Nome\n3 - Visualizar Registros Que Contenham Certa String No Nome\n4 - Visualizar Os Registros em Ordem Alfabética\n5 - Sair\n");
+    printf("\n\nTecle a Opcao Escolhida: ");
+    scanf("%d", &opcao);
+    return opcao;
+}
 // int consulta_lista(Lista *li, int pos, struct contato *cont)
 // {
 //     if (li == NULL || pos <= 0)
